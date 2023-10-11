46f49d3ea4d2e338fc02462bae03662014bfc93a
Trix <93526043+trixoniisama@users.noreply.github.com>
2023-10-11 19:51:13 +0200

    Fix last commit mixing an argument

--- av1an-core/src/scene_detect.rs
+++ av1an-core/src/scene_detect.rs
@@ -14,6 +14,7 @@ use crate::{into_smallvec, progress_bar, Encoder, Input, ScenecutMethod, Verbosi
 
 pub fn av_scenechange_detect(
   input: &Input,
+  input_sc: Option<Input>,
   encoder: Encoder,
   total_frames: usize,
   min_scene_len: usize,
